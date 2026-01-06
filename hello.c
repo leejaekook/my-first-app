8fbc47db9178746aad6532b0bff46d9dd5cc550f
leejaekook <choice30@gmail.com>
2026-01-06 11:59:27 +0900

    hello.c edit

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main() {
     // printf is used to print output to the console.
-    printf("Hello, C Programming World!\n");
+    printf("Hello,******* C Programming World!\n");
 
     // 'main' function returns 0 to indicate successful execution.
     return 0;
